69cbd31570192c07add2b99031a9db96203ece62
Mihir Parikh <parikhmkp@gmail.com>
2017-02-18 20:04:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.get('/', function (req, res) {   // Handling specific URL's
 app.get('/Article-one',function (req, res) {
      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
+app.get('/Article-two',function (req, res) {
+     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+app.get('/Article-three',function (req, res) {
+     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
