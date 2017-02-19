12f72341ca10960cd6ff6827b2066330e5b464a0
Mihir Parikh <parikhmkp@gmail.com>
2017-02-19 10:21:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element=document.getElementById('message');
 element.innerHTML='Be patient, articles will be deployed soon.';
+// moving the image in profile.html
+var image=document.getElementById('image');
+image.onclick=function (){
+    image.style.marginLeft='10px';
+};
