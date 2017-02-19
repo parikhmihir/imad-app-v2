ebc88f1c246c459e3bc28d3a3a89722c4006ceb0
Mihir Parikh <parikhmkp@gmail.com>
2017-02-19 10:26:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,8 +88,8 @@ app.get('/ui/mihir.jpg', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-app.get('/', function (req, res) {   // Handling specific URL's
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+app.get('/profile.html', function (req, res) {   // Handling specific URL's
+  res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
 });
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
