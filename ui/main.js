c3f76e618e5fee15e1eb2b9d09ea64883a9b2593
Mihir Parikh <parikhmkp@gmail.com>
2017-02-19 10:14:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
 var element=document.getElementById('message');
-element.innerHTML='Be patient, articles will be deployed soon.'
+element.innerHTML='Be patient, articles will be deployed soon.';
