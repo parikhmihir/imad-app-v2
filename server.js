f8773c116562abc516b28295c39578039069f241
Mihir Parikh <parikhmkp@gmail.com>
2017-02-21 10:27:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,6 +68,11 @@ function createTemplate(data) {
 return htmlTemplate;
 }
 
+var counter=0;
+app.get('/counter',function(req,res){
+   counter=counter+1;
+   counter.toString();
+});
 app.get('/:articleName',function (req, res) { //:articleName converts name into variables,its a property of "express"
     //articleName==articleOne
     //articles[articleName]=={}content object of article one.
