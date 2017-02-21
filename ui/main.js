61eb144c575440128ef3d4862cb4928f441030e8
Mihir Parikh <parikhmkp@gmail.com>
2017-02-21 09:54:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,12 @@
-console.log('Loaded!');
-var element=document.getElementById('message');
-element.innerHTML='Be patient, articles will be deployed soon.';
+//counter code
+var counter=document.getElementById('counter');
+var span=document.getElementById('count');
+var counter=0;
+button.onclick=function(){
+  // Make a request to counter endpoint
+  
+  //Capture the response and store it in a variable
+  
+  //Render teh variable in correct span
+  
+};
