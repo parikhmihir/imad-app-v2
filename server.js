d8c1bc22ab2071b70cc7475e61ba20794954c8de
Mihir Parikh <parikhmkp@gmail.com>
2017-02-25 09:51:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.get('/test-db',function(req,res){ //request to an end point
        if(err){
            res.status(500).send(err.toString());
        }else{
-           res.send(JSON.stringify(result));
+           res.send(JSON.stringify(result.rows));
        }
    });
 });
