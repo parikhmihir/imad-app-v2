4ac89b58c14eb6af33b7c840ff54ff88bf19623f
Mihir Parikh <parikhmkp@gmail.com>
2017-02-25 10:05:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,11 +89,20 @@ app.get('/test-db',function(req,res){ //request to an end point
 });
     
 
-app.get('/:articleName',function (req, res) { //:articleName converts name into variables,its a property of "express"
+app.get('articles/:articleName',function (req, res) { //:articleName converts name into variables,its a property of "express"
     //articleName==articleOne
     //articles[articleName]=={}content object of article one.
-    var articleName=req.params.articleName;
-     res.send(createTemplate(articles[articleName]));
+    Pool.query("Select * from articles where title= "+ req.params.articleName, function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            if(result.rows.length===0){
+                res.status(404).send('Article not found');
+            } else {var articleData=result.rows[0];
+        }
+        }
+    });
+     res.send(createTemplate(articleData));
 });
 
 app.get('/', function (req, res) {   // Handling specific URL's
