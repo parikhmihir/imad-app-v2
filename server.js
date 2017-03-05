e578cc480ca0784fbb34a128ad768d2d5d1ae5c4
Mihir Parikh <parikhmkp@gmail.com>
2017-03-05 20:05:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,30 +106,30 @@ app.post('/add-user',function(req,res){
 });
 
 // Logging in the user
-app.post('/login',function(req,res){
-    var username=req.body.username;
-    var password=req.body.password;
-     Pool.query('SELECT * FROM "user" WHERE username=$1' [username], function (err, result) {
-    if(err){
-           res.status(500).send(err.toString());
-       }else{
-           if(result.rows.length===0){
-               res.send(403).send('Username/Password is invalid ');
-           }
-           else{
-               // Match the password
-               var dbString=result.rows[0].password;
-               var salt=dbString.split('$')[2];
-               var hashedPassword=hash(password,salt);
-               if(hashedPassword===dbString){
-               res.send(' Successful Login for '+username);
-               }
-               else{
-                   res.send(403).send('Username/Password is invalid ');
-               }
-           }
-       }    
-    });
+app.post('/login', function (req, res) {
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   Pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          if (result.rows.length === 0) {
+              res.status(403).send('username/password is invalid');
+          } else {
+              // Match the password
+              var dbString = result.rows[0].password;
+              var salt = dbString.split('$')[2];
+              var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
+              if (hashedPassword === dbString) {
+                res.send('credentials correct!');
+                
+              } else {
+                res.status(403).send('username/password is invalid');
+              }
+          }
+      }
+   });
 });
 
 
