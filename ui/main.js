d0eb65c4ccbb0848dca0ebd7c59081e538c00936
Mihir Parikh <parikhmkp@gmail.com>
2017-03-05 20:35:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,7 @@
 var submit=document.getElementByID('submit');
-var nameInput=document.getElementByID('username');
-var name=nameInput.value;
+var username=document.getElementByID('username').value;
+var password=document.getElementByID('password').value;
+console.log(username);
+console.log(password);
 request.open('POST','http://parikhmihir.imad.hasura-app.io/login');
 request.send(JSON.stringify({username:username, password:password}));
