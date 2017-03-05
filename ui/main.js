fe6ce7ac0f722e1974107c8ca9dfe75703bcab1f
Mihir Parikh <parikhmkp@gmail.com>
2017-03-05 21:14:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-var submit=document.getElementById('login_btn');
+var submit=document.getElementById('submit');
 submit.onclick = function () {
         // Create a request object
         var request = new XMLHttpRequest();
