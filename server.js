a7b8c475d08de7fdaef8dd78aecf6bc85a9342fb
Mihir Parikh <parikhmkp@gmail.com>
2017-03-05 18:51:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ return htmlTemplate;
 // Creating the hashed value of our credentials
 function hash(input,salt){
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return [pbkdf2,salt,hashed.toStrting('hex')].join('$');
+    return ['pbkdf2',10000,salt,hashed.toString('hex')].join('$');
 }
 
 app.get('/hash/:input', function(req,res){
