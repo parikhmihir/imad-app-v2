26147584e8a69387e90fb07d10dd390177b5bc09
Mihir Parikh <parikhmkp@gmail.com>
2017-03-07 20:29:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,4 +32,4 @@
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username, password: password}));  
         submit.value = 'Logging in...';
-    };
+    }
