8be02e94b9c115f8ef65d00687f4f863b83b5c56
Mihir Parikh <parikhmkp@gmail.com>
2017-03-07 20:23:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,34 +1,35 @@
-function login1(){
-    var submit=document.getElementById('submit');
-        submit.onclick = function () {
+  // Submit username/password to login
+    var submit = document.getElementById('login_btn');
+    submit.onclick = function () {
         // Create a request object
-            var request = new XMLHttpRequest();
-            
-            // Capture the response and store it in a variable
-            request.onreadystatechange = function () {
-              if (request.readyState === XMLHttpRequest.DONE) {
-                  // Take some action
-                  if (request.status === 200) {
-                    console.log('User Logged in...');
-                    alert('Logged in successfully');
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Sucess!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
               }
-              else if (request.status===403){
-                  alert('Username/password is incorrect');
-            }
-            else if (result.status===500){
-                alert('Something went wrong with the server');
-            }
-              // Not done yet
-            }
-            // Make the request
-            var username = document.getElementById('username').value;
-            var password = document.getElementById('password').value;
-            console.log(username);
-            console.log(password);
-            request.open('POST', '/login', true);
-            request.setRequestHeader('Content-Type', 'application/json');
-            request.send(JSON.stringify({username: username, password: password}));  
-            submit.value = 'Logging in...';
+          }  
+          // Not done yet
         };
-    };   
-    }
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+    };
