2de7afa83d3439c3e5d116a42bf0f6fc65f8557c
Mihir Parikh <parikhmkp@gmail.com>
2017-03-14 22:52:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,6 +83,21 @@ function createTemplate(data) {
 return htmlTemplate;
 }
 
+app.get('/articles/:articleName', function (req, res) {
+  // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
+  Pool.query("SELECT * FROM articles WHERE title = $1", [req.params.articleName], function (err, result) {
+    if (err) {
+        res.status(500).send(err.toString());
+    } else {
+        if (result.rows.length === 0) {
+            res.status(404).send('Article not found');
+        } else {
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+        }
+    }
+  });
+});
 // Creating the hashed value of our credentials
 function hash(input,salt){
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
@@ -235,22 +250,6 @@ app.post('/submit-comment/:articleName', function (req, res) {
    });
 });*/
     
-
-app.get('/articles/:articleName', function (req, res) {
-  // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
-  Pool.query("SELECT * FROM articles WHERE title = $1", [req.params.articleName], function (err, result) {
-    if (err) {
-        res.status(500).send(err.toString());
-    } else {
-        if (result.rows.length === 0) {
-            res.status(404).send('Article not found');
-        } else {
-            var articleData = result.rows[0];
-            res.send(createTemplate(articleData));
-        }
-    }
-  });
-});
 
 app.get('/', function (req, res) {   // Handling specific URL's
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
