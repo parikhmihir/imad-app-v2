129adba55a85de804b87f182c42edb74fff7c7b1
Mihir Parikh <parikhmkp@gmail.com>
2017-03-27 22:41:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -97,8 +97,22 @@ function loadLoginForm () {
 function loadLoggedInUser (username) {
     var loginArea = document.getElementById('login_area');
     loginArea.innerHTML = `
-        <h3> Hi <i>${username}</i></h3>
-        <a href="/logout">Logout</a>
+    <html>
+    <head>
+         <meta name="viewport" device="width:device-width" initial-scale="1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+         <ul>
+	    	<li class="navbar"><a href="/">HOME</a></li>
+		    <li class="navbar"><a href="/ui/profile.html">PROFILE</a></li>
+		    <li class="navbar"><a href="/ui/articles.html">ARTICLES</a></li>
+		    <li id="right" class="navbar"><a href="/ui/register.html">LOGIN/REGISTER</a></li>
+            <li>Welcome  <i>${username}</i></li>
+            <li><a href="/logout">Logout</a></li>
+        </ul>
+    </body>
+    </html>
     `;
 }
 
