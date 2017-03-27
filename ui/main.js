c65a06d283c0e8a334e6f28b84295acb8179c426
Mihir Parikh <parikhmkp@gmail.com>
2017-03-27 23:00:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -106,7 +106,6 @@ function loadLoggedInUser (username) {
 	    	<li class="navbar"><a href="/">HOME</a></li>
 		    <li class="navbar"><a href="/ui/profile.html">PROFILE</a></li>
 		    <li class="navbar"><a href="/ui/articles.html">ARTICLES</a></li>
-		    <li id="right" class="navbar"><a href="/ui/register.html">LOGIN/REGISTER</a></li>
             <li><font color="white">Welcome  <i>${username}</i></font></li>
             <li id="right" class="navbar"><a href="/ui/register.html">LOGIN/REGISTER</a></li>
             <li><a href="/logout">LOGOUT</a></li>
